b76d8401caaecfcf9411699ab00cf95da76309c2
hengweichen <108580195+hengweichen@users.noreply.github.com>
2024-11-18 02:02:18 +0800

    simple statistics
    This simple statistics display the hardcoded data

--- BIT216/dashboard.php
+++ BIT216/dashboard.php
@@ -307,35 +307,6 @@
         </main>
     </div>
 
-    <script>
-        var xValues = ["Paper", "Aluminium", "Plastic"];
-        var yValues = [10, 10, 10];
-        var barColors = [
-        "#a2df9c",
-        "#00aba9",
-        "#3a3939e9"
-        ];
 
-        new Chart("myChart", {
-        type: "pie",
-        data: {
-            labels: xValues,
-            datasets: [{
-            backgroundColor: barColors,
-            data: yValues
-            }]
-        },
-        options: {
-            title: {
-            display: true,
-            text: "Type of Waste"
-            },
-            legend: {
-            display: true,
-            position: 'right'
-            }
-        }
-        });
-    </script>
 </body>
 </html>
